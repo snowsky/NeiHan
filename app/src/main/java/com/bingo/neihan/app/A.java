2e4ea009943d97c704562bfce2406b5a76e13923
BingoKingCoding <634051075@qq.com>
2018-03-23 17:38:59 +0800

    首次提交

---
+++ app/src/main/java/com/bingo/neihan/app/A.java
@@ -0,0 +1,9 @@
+package com.bingo.neihan.app;
+
+/**
+ * <请描述这个类是干什么的>
+ * Created by WangWB on 2018/03/23  15:39.
+ */
+
+public class A {
+}
---
+++ app/src/main/java/com/bingo/neihan/app/base/BaseFragment.java
@@ -0,0 +1,70 @@
+package com.bingo.neihan.app.base;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.util.Log;
+import android.view.View;
+
+/**
+ * <Fragment基类，封装了懒加载的实现>
+ * Created by WangWB on 2018/03/23  15:40.
+ *
+ * Viewpager + Fragment情况下，fragment的生命周期因Viewpager的缓存机制而失去了具体意义
+ * 该抽象类自定义新的回调方法，
+ * 方法1、当fragment可见状态改变时会触发的回调方法
+ * 方法2、fragment第一次可见时会回调的方法(避免重复加载数据)
+ */
+
+public abstract class BaseFragment extends Fragment{
+
+    protected final String TAG = this.getClass().getSimpleName();
+
+    private boolean isViewCreated;  //Fragment的View加载完毕的标记
+    private boolean isVisibleToUser;//fragment是否显示了
+    private boolean isDataInitiated;//是否已经数据初始化
+
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        isViewCreated = true;
+        lazyLoad();
+    }
+
+    /**
+       * Description:
+       * 该方法在fragment创建的时候(未显示的时候)会先被调用一次(在OnCreateView方法之前)，
+       * 此时方法的形参isVisibleToUser = false
+       * 当前Fragment可见时，该方法会再次被调用一次(还是在OnCreateView方法之前)，
+       * 此时方法的形参isVisibleToUser = true
+       */
+    @Override
+    public void setUserVisibleHint(boolean isVisibleToUser) {
+        super.setUserVisibleHint(isVisibleToUser);
+
+        Log.d("TAG", TAG + " setUserVisibleHint() --> isVisibleToUser = " + isVisibleToUser);
+
+        this.isVisibleToUser = isVisibleToUser;
+        lazyLoad();
+    }
+
+    public boolean lazyLoad() {
+        return lazyLoad(false);
+    }
+     /**
+       * Description:
+       *    forceUpdate为强制更新数据使用,避免只执行一次
+       */
+    public boolean lazyLoad(boolean forceUpdate){
+        if (isVisibleToUser && isViewCreated && (!isDataInitiated || forceUpdate)) {
+            lazyInitData();
+            isDataInitiated = true;
+            return true;
+        }
+        return false;
+    }
+
+    protected abstract void lazyInitData();
+
+}
